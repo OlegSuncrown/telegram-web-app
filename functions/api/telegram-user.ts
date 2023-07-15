d2a7e1443f4551c9c13c83dcc14cd80e1bf72791
Oleh <unknown@gmail.com>
2023-07-15 09:35:21 +0300

    add supabase client

---
+++ functions/api/telegram-user.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js';
+
+export async function onRequestPost({ env, request }: any) {
+  const payload = await request.json();
+  const { query_id, chat_id, user_id, is_bot, first_name, last_name, username, photo_url } = payload?.telegramUser;
+  const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_SECRET_KEY);
+  const { data, error } = await supabase
+    .from('telegram-users')
+    .insert([
+      {
+        query_id,
+        chat_id,
+        user_id,
+        is_bot,
+        first_name,
+        last_name,
+        username,
+        photo_url,
+      },
+    ])
+    .select();
+  if (error) throw error;
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
